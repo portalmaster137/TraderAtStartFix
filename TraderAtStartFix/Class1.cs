d2b53961ad8c2f68a010e04e13ecf2a195e89a85
zen dor-colley <portalmaster137@gmail.com>
2021-10-31 22:41:42 +1000

    Add project files.

---
+++ TraderAtStartFix/Class1.cs
@@ -0,0 +1,77 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using UnityEngine;
+using DiskCardGame;
+using HarmonyLib;
+
+namespace TraderAtStartFix
+{
+	[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+	public class Plugin : BaseUnityPlugin
+	{
+		private const string PluginGuid = "bobthenerd.inscryption.traderstart";
+		private const string PluginName = "TraderAtStart";
+		private const string PluginVersion = "1.0.0";
+
+		internal static ManualLogSource Log;
+
+		private void Awake()
+		{
+			Logger.LogInfo($"Loaded {PluginName}!");
+			Plugin.Log = base.Logger;
+
+			Harmony harmony = new Harmony(PluginGuid);
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(DeckInfo), "InitializeAsPlayerDeck")]
+		public class TraderStartPatch : DeckInfo
+		{
+			[HarmonyPrefix]
+			public static bool Prefix(ref DeckInfo __instance)
+            {
+				__instance.AddCard(CardLoader.GetCardByName("PeltHare"));
+				__instance.AddCard(CardLoader.GetCardByName("PeltHare"));
+				__instance.AddCard(CardLoader.GetCardByName("PeltHare"));
+				__instance.AddCard(CardLoader.GetCardByName("PeltHare"));
+				if (SeededRandom.Range(1,3, SaveManager.SaveFile.GetCurrentRandomSeed()) == 1)
+                {
+					__instance.AddCard(CardLoader.GetCardByName("PeltWolf"));
+				}
+				if (SeededRandom.Range(1, 10, SaveManager.SaveFile.GetCurrentRandomSeed()) == 1)
+				{
+					__instance.AddCard(CardLoader.GetCardByName("PeltGolden"));
+				}
+				return false;
+			}
+
+		}
+
+		[HarmonyPatch(typeof(PaperGameMap), "TryInitializeMapData")]
+		public class RunState_TryInitializeMapData
+        {
+			public static bool Prefix(ref PaperGameMap __instance)
+            {
+				if (RunState.Run.map == null)
+				{
+					PredefinedNodes nodes = ScriptableObject.CreateInstance<PredefinedNodes>();
+					PredefinedScenery scenery = ScriptableObject.CreateInstance<PredefinedScenery>();
+					var node1 = new NodeData();
+					var node2 = new TradePeltsNodeData();
+					nodes.nodeRows.Add(new List<NodeData>() { node1 });
+					nodes.nodeRows.Add(new List<NodeData>() { node2 });
+					RunState.Run.map = MapGenerator.GenerateMap(RunState.CurrentMapRegion, 3, 13, nodes, scenery);
+					RunState.Run.currentNodeId = SaveManager.SaveFile.currentRun.map.RootNode.id;
+
+				}
+				return false;
+			}
+        }
+	}
+}
